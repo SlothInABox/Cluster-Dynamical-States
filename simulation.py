0f0c4e81da3ba4ea36c04c7da5d434764aa853d1
James Goodman <s1626718@cplab228.ph.ed.ac.uk>
2020-01-20 14:22:59 +0000

    updated simulation.py

--- simulation.py
+++ simulation.py
@@ -1,3 +1,19 @@
-"""
+"""simulation file
+
+This module uses parameter data for assorted galaxy clusters to simulate
+clusters at various timesteps. The data can be analysed to determine whether a
+cluster is relaxed or unrelaxed. The timescale for a galaxy to go from relaxed
+to unrelaxed can then be determined.
+
+To do:
+    *Import data from 'dat' folder
+    *Create graphs of parameters for test cluster (zero redshift)
 
 """
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
