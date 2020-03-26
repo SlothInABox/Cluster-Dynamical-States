788f17319f54b4520019a6176582417feb8e7366
James Goodman <jgoo1102@gmail.com>
2020-03-26 17:46:53 +0000

    Changed read in method. Added distribution of timescales.

--- .ipynb_checkpoints/utils-checkpoint.py
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -15,55 +15,81 @@ from scipy.interpolate import make_interp_spline, BSpline
 from scipy.signal import find_peaks
 
 def read_data(path):
-    """Function to read in data.
-
-    Reads in files located in the 'dat' folder. Determines redshift from the
-    filename and redshifts.txt file. Stores file data in dicitonarys of
-    ndarrays.
-
-    Any data rows which contain a NaN value or a negative value are incorrect
-    and therefore removed from the data.
-
-    R200 and R500 data is stored in separate dictionarys. Follows following
-    format:
-        [0]: Hid
-        [1]: eta
-        [2]: delta
-        [3]: fm
-
+    """Method for reading in data files.
+    
+    Reads in files located in data folder. Determines redshift from the
+    filename. Stores each cluster ID as a key with eta, fm, delta, and redshift
+    data as values.
+    
     Args:
-        path (str): The folder containing the data files.
-
+        path (str): Folder containing data files.
+        
     Returns:
-        R200 (dict): R200 cluster data. Keys are the red shifts of the files.
-            Data is an ndarray containing parameters.
-        R500 (dict): R500 cluster data. Keys are the red shifts of the files.
-            Data is an ndarray containing parameters.
-
+        clusters (dict): Dict of ndarrays containing data for each cluster.
+    
     """
-    #: list of str: Files and directories in defined path.
+    #: list of str: Files and directories in path.
     entries = os.listdir(path)
-    #:  np array: list of redshift data and snapnums.
+    
+    #: ndarray: Redshift data and snapnums.
     rshifts = np.loadtxt('redshifts.txt', delimiter=' ')
     #: Correct redshift data so that negative values become 0 redshift.
     rshifts[rshifts < 0.0] = 0.0
-    #: dicts: Empty dictionarys for R200 and R500 data.
-    R200, R500 = {}, {}
+    
+    #: dict: Empty dictionary for cluster data.
+    clusters = {}
     for entry in entries:
         print('---Reading: {}---'.format(entry), end='\r')
         #: int: Get snapnum from filename.
-        snap_num = int(entry.split('_')[3])
-        #: flt: Cross referenced red shift value.
-        rshift = rshifts[np.where(rshifts[:,0] == snap_num)][0][2]
+        snapnum = int(entry.split('_')[3])
+        #: flt: Cross reference rshift value.
+        rshift = rshifts[np.where(rshifts[:,0] == snapnum)][0][2]
+        #: ndarray: Loaded data.
         entry_data = np.loadtxt(path + entry)
+        #: Remove invalid data.
         entry_data = entry_data[np.logical_and(
                                                ~np.isnan(entry_data).any(axis=1),
                                                np.all(entry_data >= 0, axis=1)
                                                )]
-        R200[rshift] = np.copy(entry_data[:, [0, 3, 4, 5]])
-        R500[rshift] = np.copy(entry_data[:, [0, 8, 9, 10]])
+        for cluster in entry_data:
+            if clusters.get(cluster[0]) == None:
+                clusters[cluster[0]] = []
+            clusters[cluster[0]].append([cluster[3], cluster[4], cluster[5], rshift])
     print()
-    return(R200, R500)
+    for idx in clusters.keys():
+        clusters[idx] = np.array(clusters[idx])
+    rshifts = np.copy(rshifts[:,2])[::-1]
+    return(clusters, rshifts)
+
+def pull_rshift_set(cluster_data, rshift):
+    """Method for pulling all cluster data from specific rshift.
+    
+    Args:
+        cluster_data (dict): All cluster data.
+        rshift (flt): Redshift value to be grabbed.
+    
+    Returns:
+        eta (ndarray): All eta values.
+        delta (ndarray): All delta values.
+        fm (ndarray): All fm values.
+    
+    """
+    #: list: Store eta, delta, and fm values.
+    eta, delta, fm = [], [], []
+    # Iterate over the cluster.
+    for _, cluster in cluster_data.items():
+        #: ndarray: Select only correct rshift data.
+        data = cluster[cluster[:,3] == rshift]
+        if data.size != 0:
+            #: Append eta, delta, and fm values.
+            eta.append(data[:,0][0])
+            delta.append(data[:,1][0])
+            fm.append(data[:,2][0])
+    #: Turn lists into arrays.
+    eta = np.array(eta)
+    delta = np.array(delta)
+    fm = np.array(fm)
+    return(eta, delta, fm)
 
 def fit_data(x, y):
     """Method for fitting data from a file.
@@ -213,13 +239,12 @@ def spline(x, y):
     power_smooth = spl(xgrid)
     return(xgrid, power_smooth)
 
-def track_relaxation(cluster_idx, data):
+def track_relaxation(cluster_data, cluster_idx):
     """Function for tracking the relaxation of a cluster.
     
     Args:
-        cluster_idx (int): Index of the target cluster.
-        data (dict): Contains parameter data. Keys are redshifts, values
-            are ndarrays containing parameter data.
+        cluster_data (dict): All cluster data.
+        cluster_idx (int): ID of specific cluster.
             
     Returns:
         r (ndarray): Relaxation parameters for the cluster.
@@ -227,21 +252,88 @@ def track_relaxation(cluster_idx, data):
         rshifts (ndarray): Corresponding redshift values.
     
     """
-    #: list: Stores relaxation values.
-    r_cols = []
-    
-    for rshift in sorted(data.keys()):
-        #: flts: Parameters for calculating theta and relaxation.
-        eta, delta, fm = data[rshift][cluster_idx,1], data[rshift][cluster_idx,2], data[rshift][cluster_idx,3]
-        
-        #: flts: Calculated theta and uncertainty.
-        theta, theta_err = calc_theta(fm, delta)
-        #: flts: Calculated r and uncertainty.
-        r, r_err = calc_r(theta, theta_err, eta)
-        r_cols.append([r, r_err, rshift])
-        
-    #: ndarray: Convert r_cols into ndarray.
-    r_cols = np.array(r_cols)
-    #: ndarrays: Point to new parameters.
-    r, r_err, rshift = r_cols[:,0], r_cols[:,1], r_cols[:,2]
+    cluster = cluster_data[cluster_idx]
+    eta, delta, fm, rshift = cluster[:,0], cluster[:,1], cluster[:,2], cluster[:,3]
+    rshift_idx = rshift.argsort()
+    eta, delta, fm, rshift = eta[rshift_idx], delta[rshift_idx], fm[rshift_idx], rshift[rshift_idx]
+
+    # Calculate theta.
+    theta, theta_err = calc_theta(fm, delta)
+    # Calculate r.
+    r, r_err = calc_r(theta, theta_err, eta)
     return(r, r_err, rshift)
+
+def peak_limits(x, y, threshold, ax=None):
+    """Function for getting the limits either side of peaks.
+    
+    Recieves a data set in the form of x and y and identifies the peaks of the data.
+    Then iterates over the data either side of the peak and finds the first points
+    where the data dips below the threshold value. Returns the two dip indexes and
+    also plots the points on an axis if indicated.
+    
+    Args:
+        x (ndarray): X values of the data.
+        y (ndarray): Y values of the data.
+        threshold (flt): Relaxation threshold value.
+        ax (axis): Axis for data to be plotted on. Defaults to 'None'.
+    
+    Returns:
+        dips (ndarray): Contains left and right dips of peaks.
+        
+    """
+    #: ndarray: Index of peak values of data.
+    peaks, _ = find_peaks(y, prominence=0.5)
+    # Plot peaks.
+    if ax != None:
+        ax.plot(x[peaks], y[peaks], 'x')
+        
+    #: list: Store dip indexes.
+    dips = [] 
+    # Iterate over peaks.
+    for peak in peaks:
+        # Only use peaks found above the threshold.
+        if y[peak] >= threshold:
+            #: int: Counter for iteration.
+            counter = 0
+            #: int: Index position of left and right dip. Default 'None'
+            left_dip, right_dip = None, None
+            
+            while left_dip == None or right_dip == None:
+                try:
+                    if y[peak + counter] <= threshold and right_dip == None:
+                        right_dip = peak + counter
+                except:
+                    right_dip = 'NO LIMIT'
+                try:
+                    if y[peak - counter] <= threshold and left_dip == None:
+                        left_dip = peak - counter
+                except:
+                    left_dip = 'NO LIMIT'
+                counter += 1
+            if left_dip != 'NO LIMIT' and right_dip != 'NO LIMIT':
+                dips.append([left_dip, right_dip])
+            if ax != None:
+                if left_dip != 'NO LIMIT':
+                    ax.plot(x[left_dip], y[left_dip], 'x', color='green')
+                if right_dip != 'NO LIMIT':
+                    ax.plot(x[right_dip], y[right_dip], 'x', color='green')
+    #: ndarray: Transform dips into an ndarray for ease.
+    dips = np.array(dips)
+    return(dips)
+
+def lookback_time(z):
+    """Function for converting redshifts into lookback times.
+    
+    Uses: (2/(3Ho) * (1 - 1/(1+z)^(3/2))
+    
+    Args:
+        z (flt): Redshift.
+        
+    Returns:
+        t (flt): Timescale in Gyrs.
+    
+    """
+    #: flt: Hubble constant.
+    Ho = 72
+    t = (2/(3*Ho)) * (1 - 1/((1+z)**(3/2)))
+    return(t)
